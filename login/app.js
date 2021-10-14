63e94d6b27515a7bdb78b8345eb0702dac8a0e23
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2021-10-14 08:37:55 +0700

    menambah animasi pada class input-field

--- login/app.js
+++ login/app.js
@@ -5,6 +5,7 @@ inputs.forEach( inp => {
         inp.classList.add("active");
     });
     inp.addEventListener("blur", () => {
+        if(inp.value != "") return;
         inp.classList.remove("active");
     })
 });
